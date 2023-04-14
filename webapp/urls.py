b333293075451c1702b9279921a7ccb03bc60d61
Valentin Kim <valentink@yandex.ru>
2023-04-14 18:44:33 +0600

    доработки по PEP 8

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 
-from webapp.views.task_1_and_2 import add, subtract, multiply, divide, get_token_view, IndexView
+from webapp.views.task_1_and_2 import (
+    add,
+    subtract,
+    multiply,
+    divide,
+    get_token_view,
+    IndexView,
+)
 
 urlpatterns = [
-    path("", IndexView.as_view(), name='index'),
-    path("add/", add, name='add'),
-    path("subtract/", subtract, name='subtract'),
-    path("multiply/", multiply, name='multiply'),
-    path("divide/", divide, name='divide'),
-    path('token/', get_token_view, name='token'),
+    path("", IndexView.as_view(), name="index"),
+    path("add/", add, name="add"),
+    path("subtract/", subtract, name="subtract"),
+    path("multiply/", multiply, name="multiply"),
+    path("divide/", divide, name="divide"),
+    path("token/", get_token_view, name="token"),
 ]
--- webapp/views/task_1_and_2.py
+++ webapp/views/task_1_and_2.py
@@ -1,34 +1,35 @@
 from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
 import json
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.views.generic import TemplateView, RedirectView
+from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = "index.html"
 
 
 @ensure_csrf_cookie
 def get_token_view(request, *args, **kwargs):
-    if request.method == 'GET':
+    if request.method == "GET":
         return HttpResponse()
-    return HttpResponseNotAllowed(f'Only GET request are allowed {request.method}')
+    return HttpResponseNotAllowed(
+        f"Only GET request are allowed {request.method}"
+    )
 
 
 @csrf_exempt
 def add(request, *args, **kwargs):
-    if request.method == 'POST' and request.body:
-        print(request.body)
+    if request.method == "POST" and request.body:
         try:
             fields = json.loads(request.body)
-            print(fields)
-            sum = int(fields['A']) + int(fields['B'])
-            print(sum)
+            sum = int(fields["A"]) + int(fields["B"])
             answer_as_json = json.dumps({"answer": sum}, indent=2)
-            response = HttpResponse(answer_as_json, content_type='application/json')
+            response = HttpResponse(
+                answer_as_json, content_type="application/json"
+            )
         except Exception:
-            response_data = {'detail': 'Некорректный набор данных'}
+            response_data = {"detail": "Некорректный набор данных"}
             response = JsonResponse(response_data)
             response.status_code = 400
         return response
@@ -36,14 +37,16 @@ def add(request, *args, **kwargs):
 
 @csrf_exempt
 def subtract(request, *args, **kwargs):
-    if request.method == 'POST' and request.body:
+    if request.method == "POST" and request.body:
         try:
             fields = json.loads(request.body)
-            sub = int(fields['A']) - int(fields['B'])
+            sub = int(fields["A"]) - int(fields["B"])
             answer_as_json = json.dumps({"answer": sub}, indent=2)
-            response = HttpResponse(answer_as_json, content_type='application/json')
+            response = HttpResponse(
+                answer_as_json, content_type="application/json"
+            )
         except Exception:
-            response_data = {'detail': 'Некорректный набор данных'}
+            response_data = {"detail": "Некорректный набор данных"}
             response = JsonResponse(response_data)
             response.status_code = 400
         return response
@@ -51,14 +54,16 @@ def subtract(request, *args, **kwargs):
 
 @csrf_exempt
 def multiply(request, *args, **kwargs):
-    if request.method == 'POST' and request.body:
+    if request.method == "POST" and request.body:
         try:
             fields = json.loads(request.body)
-            mult = int(fields['A']) * int(fields['B'])
+            mult = int(fields["A"]) * int(fields["B"])
             answer_as_json = json.dumps({"answer": mult}, indent=2)
-            response = HttpResponse(answer_as_json, content_type='application/json')
+            response = HttpResponse(
+                answer_as_json, content_type="application/json"
+            )
         except Exception:
-            response_data = {'detail': 'Некорректный набор данных'}
+            response_data = {"detail": "Некорректный набор данных"}
             response = JsonResponse(response_data)
             response.status_code = 400
         return response
@@ -66,20 +71,19 @@ def multiply(request, *args, **kwargs):
 
 @csrf_exempt
 def divide(request, *args, **kwargs):
-    if request.method == 'POST' and request.body:
-
+    if request.method == "POST" and request.body:
         try:
             fields = json.loads(request.body)
-            div = int(fields['A']) / int(fields['B'])
+            div = int(fields["A"]) / int(fields["B"])
             answer_as_json = json.dumps({"answer": div}, indent=2)
-            response = HttpResponse(answer_as_json, content_type='application/json')
+            response = HttpResponse(
+                answer_as_json, content_type="application/json"
+            )
         except ZeroDivisionError as e:
             response_data = {"error": "Division by zero!"}
             response = JsonResponse(response_data)
         except Exception:
-            response_data = {'detail': 'Некорректный набор данных'}
+            response_data = {"detail": "Некорректный набор данных"}
             response = JsonResponse(response_data)
             response.status_code = 400
         return response
-
-
